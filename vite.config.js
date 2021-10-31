1e32c559d490e452482b616dc6048c704665c745
aleksanderwalczuk <ohleg@windowslive.com>
2021-10-31 10:56:25 +0100

    init

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VitePluginFonts from 'vite-plugin-fonts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePluginFonts({
+      google: {
+        families: [{
+          name: 'Open Sans Condensed',
+          styles: 'wght@700',
+          defer: true,
+        },
+        {
+          name: 'Lora',
+          styles: 'wght@400;500;600;700',
+        }
+      ]
+      }
+    })
+  ]
+})
