f33147d13e338ae31cad9aa6e89f5f00ed2d8c86
aleksanderwalczuk <ohleg@windowslive.com>
2021-10-31 13:00:37 +0100

    fix bundle

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import VitePluginFonts from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nartygdanskpl/',
   plugins: [
     vue(),
     VitePluginFonts({
