71802daf1f8054a0ef1a731e723d1574c607794e
aleksanderwalczuk <ohleg@windowslive.com>
2021-10-31 12:43:30 +0100

    automation test

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,21 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        red: {
+          'default': '#FF6B6B',
+          '300': '#FFBABA',
+          '400': '#FF8F8F',
+          '500': '#FF6B6B',
+          '600': '#FF4C4C',
+          '700': '#E92525',
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
 }
